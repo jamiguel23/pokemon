ca05e153fb1ec7228409d9585f155a7dda0d83b4
Matt <josematthewmiguel@gmail.com>
2022-04-21 22:24:22 -0700

    added main componet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './component/Header';
 import './App.css';
 import Footer from './component/Footer';
+import Main from './component/Main';
 
 function App() {
   return (
     <div className="App">
       <div className='TitleSection'>
         <Header />
+        <Main />
         <Footer />
       </div>
     </div>
