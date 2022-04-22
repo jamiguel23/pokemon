547394567df51e829ca0690115bfb4bcd85e33c2
Matt <josematthewmiguel@gmail.com>
2022-04-21 22:22:16 -0700

    added footer componet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './component/Header';
 import './App.css';
+import Footer from './component/Footer';
 
 function App() {
   return (
     <div className="App">
       <div className='TitleSection'>
         <Header />
+        <Footer />
       </div>
     </div>
   );
