3531e7334c19894924fa66bc819ea819508c6f48
Matt <josematthewmiguel@gmail.com>
2022-04-23 23:10:19 -0700

    note

--- src/component/Main.js
+++ src/component/Main.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Main extends Component {
   render() {
     return (
-      <div>Main content here</div>
+      <div>Main content will go here</div>
     )
   }
 }
